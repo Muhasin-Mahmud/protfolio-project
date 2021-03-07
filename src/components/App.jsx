6216cf673ac7f1b97c1a7713d0110f38440bff1c
Muhasin Mahmud <muhasinuddinmahmud@gmail.com>
2021-03-07 18:05:37 +0100

    project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
+  return (
+    <div>
+    <h2>Portfolio Project</h2>
+    </div>
+  )
+}
+
+export default App
